a9da393556a609ac39691ad3ab44f5c2fca56572
DRGNCoDR <rayfradella@yahoo.com>
2024-12-16 12:59:18 -0500

    Initial commit

---
+++ ToDo/src/app.svelte
@@ -0,0 +1,60 @@
+<script>
+    import AddItem from "./comp/add-item.svelte"
+    import Load from "./comp/Load.svelte"
+    import Settings from "./comp/settings.svelte"
+
+    let showHome = true
+    let showSettings = false
+    let showLoad = false
+    let showAdd = false
+
+    const settings = () =>  {
+        showSettings = true
+        showLoad = false
+        showAdd = false
+        showHome = false
+    }
+
+    const load = () =>  {
+        showSettings = false
+        showLoad = true
+        showAdd = false
+        showHome = false
+    }
+
+    const add = () =>  {
+        showSettings = false
+        showLoad = false
+        showHome = false
+        showAdd = true
+    }
+</script>
+{#if showHome}
+    <div>
+        <h1>ToDo App</h1>
+
+        <button on:click="{settings}">
+            Settings
+        </button>
+
+        <button on:click="{load}">
+            Load
+        </button>
+
+        <button on:click="{add}">
+            Add New
+        </button>
+    </div>
+{/if}
+
+{#if showSettings}
+    <Settings />
+{/if}
+
+{#if showLoad}
+    <Load />
+{/if}
+
+{#if showAdd}
+    <AddItem />
+{/if}
